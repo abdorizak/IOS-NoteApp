0060fc3007340d4f2b774bcd85ca1bce71dbe16f
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2021-11-30 10:10:38 +0300

    Initial Commit

---
+++ Note-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Note-App
+//
+//  Created by Abdirizak Hassan on 11/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
